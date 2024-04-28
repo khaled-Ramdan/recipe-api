7540cf6f8f7a7c24007c396bf520de7250b64af6
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2024-04-29 00:17:31 +0300

    mongoose schemas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import router from "./routes/routes.js";
 import { notFound } from "./errorHandling/not-found.js";
 import globalErrorHandler from "./errorHandling/globalErrorHandler.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 5000;
@@ -18,7 +18,7 @@ app.use(globalErrorHandler);
 
 mongoose.connect("mongodb+srv://user:user123@cluster0.rqsulab.mongodb.net/test4")
     .then(() => {
-        console.log('connected to DB');
+        console.log("connected to DB");
         app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
     })
     .catch((error) => console.log(error));
--- model/model.js
+++ model/model.js
@@ -1,53 +1,50 @@
-// here is to implement our database
-
 export let recipes = [
-  {
-    id: "id1",
-    name: "makarona bashamel",
-    price: 90,
-    chef_name: "ahmed",
-	chef_id: "chefId1",
-    Description: "A brief description or summary of the recipe.",
-    ingredients: ["meet", "makrona", "bashamel", "milk"],
-    instructions: " Step-by-step instructions on how to prepare the recipe. ",
-    cooking_time: 20,
-    Calories: 150,
-  },
-  {
-    id: "id2",
-    name: "test",
-    price: 50,
-    chef_name: "yara",
-	chef_id: "chefId2",
-    Description: "A brief description or summary of the recipe.",
-    ingredients: ["meet", "makrona", "bashamel", "milk"],
-    instructions: " Step-by-step instructions on how to prepare the recipe. ",
-    cooking_time: 20,
-    Calories: 150,
-  },
+	{
+		id: "id1",
+		name: "makarona bashamel",
+		price: 90,
+		chef_name: "ahmed",
+		chef_id: "chefId1",
+		Description: "A brief description or summary of the recipe.",
+		ingredients: ["meet", "makrona", "bashamel", "milk"],
+		instructions: " Step-by-step instructions on how to prepare the recipe. ",
+		cooking_time: 20,
+		Calories: 150,
+	},
+	{
+		id: "id2",
+		name: "test",
+		price: 50,
+		chef_name: "yara",
+		chef_id: "chefId2",
+		Description: "A brief description or summary of the recipe.",
+		ingredients: ["meet", "makrona", "bashamel", "milk"],
+		instructions: " Step-by-step instructions on how to prepare the recipe. ",
+		cooking_time: 20,
+		Calories: 150,
+	},
 ];
-
 
 export let chefs = [
 	{
-	  id: "chefId1",
-	  chef_name: "ahmed",
-	  years_of_experience : "5",
-	  recipes : ["Pancakes", "Korean Spicy Chicken Rice Noodle Bake"]
+		id: "chefId1",
+		chef_name: "ahmed",
+		years_of_experience: "5",
+		recipes: ["Pancakes", "Korean Spicy Chicken Rice Noodle Bake"]
 	},
 	{
 		id: "chefId2",
 		chef_name: "yara",
-		years_of_experience : "2",
-		recipes : ["Daffodil Cake", "Boston Cream Pie Cupcakes"]
+		years_of_experience: "2",
+		recipes: ["Daffodil Cake", "Boston Cream Pie Cupcakes"]
 	}
-  ];
+];
 
 export let append_element = (ob) => {
-  recipes.push(ob);
+	recipes.push(ob);
 };
 export let getl = () => {
-  return recipes;
+	return recipes;
 };
 
 // module.exports = {setObject, getObject}
