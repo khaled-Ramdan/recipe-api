28356fcc20124cf1a7b90945f5d2646547412999
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2024-04-29 00:27:28 +0300

    delete user defined ids

--- model/recipe.js
+++ model/recipe.js
@@ -1,10 +1,6 @@
 import { Schema, model } from "mongoose";
 
 const schema = new Schema({
-    id: {
-        type: String,
-        required: true
-    },
     name: {
         type: String,
         required: true
